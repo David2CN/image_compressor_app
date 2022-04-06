460e36a75949b71a92e5bdf09fe9baf7e4e64533
David Onyeali <onyealidavidcn@gmail.com>
2022-04-06 14:30:11 +0100

    added merge function and os for reading in images

--- compressor.py
+++ compressor.py
@@ -35,7 +35,15 @@ def compress(img, compressed_img_path="compressed.jpg"):
         
         
 
+def merge(im1, im2):
+    w = im1.size[0] + im2.size[0]
+    h = max(im1.size[1], im2.size[1])
+    im = Image.new("RGBA", (w, h))
 
+    im.paste(im1)
+    im.paste(im2, (im1.size[0], 0))
+
+    return im
 
 
 
--- tester.py
+++ tester.py
@@ -8,12 +8,9 @@ Created on Wed Apr  6 13:02:04 2022
 import os
 from compressor import read_image, compress
 
-os.listdir("images")
-           
-fname1 = "gear4.jpg"
-fname2 = "rengoku.jpg"
-fname3 = "yuno.png"
-
+fnames = os.listdir("images")
+#uploaded file would be temporarily saved here
+print(fnames)
 
 def main(fname):
     
@@ -28,11 +25,11 @@ def main(fname):
     compressed_img = read_image(compressed_fpath)
     
     
-    uncompressed_img_size = round((os.stat(fpath).st_size / 1024 / 1024), 4)
-    compressed_img_size = round((os.stat(compressed_fpath).st_size / 1024 / 1024), 4)
+    uncompressed_img_size = round((os.stat(fpath).st_size / 1024**2), 4)
+    compressed_img_size = round((os.stat(compressed_fpath).st_size / 1024**2), 4)
     compression_ratio = int(100 - (100 * (compressed_img_size / uncompressed_img_size)))
     print(f"Compression Ratio: {compression_ratio}% - {fname}: {compressed_img.format}")
     
-    
-for fname in [fname1, fname2, fname3]:\
-    main(fname)
+for fname in fnames:
+    if "compressed" not in fname:
+        main(fname)
