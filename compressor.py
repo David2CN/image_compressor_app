1fd17afe4407e50319597428536ad905b6c073f8
David Onyeali <onyealidavidcn@gmail.com>
2022-04-08 11:32:55 +0100

    removed unnecessary comments

--- compressor.py
+++ compressor.py
@@ -1,12 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr  6 11:29:28 2022
-
-@author: David Onyeali
-"""
-
 from PIL import Image
 
+# "python.defaultInterpreterPath": "C:\\Users\\Babban Gona\\Anaconda3\\python.exe",
 
 def read_image(img_path):
     """Read in image and handle error if image is unreadable.
@@ -24,14 +18,15 @@ def compress(img, compressed_img_path="compressed.jpg"):
     """Compress image.
     """
     extension = img.format
+    
     if extension == "PNG":
-        img.save(compressed_img_path, "JPEG", quality=10)
+        img.save(compressed_img_path, "PNG", quality=10)
 
     elif extension == "JPEG":
-        img.save(compressed_img_path, "JPEG", quality=20)
+        img.save(compressed_img_path, "JPEG", quality=10)
         
     else:
-        img.save(compressed_img_path, "JPEG", quality=20)
+        img.save(compressed_img_path, "JPEG", quality=10)
         
         
 
--- tester.py
+++ tester.py
@@ -1,35 +1,38 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr  6 13:02:04 2022
-
-@author: Babban Gona
-"""
-
 import os
-from compressor import read_image, compress
+from compressor import read_image, compress, merge
 
 fnames = os.listdir("images")
 #uploaded file would be temporarily saved here
-print(fnames)
 
-def main(fname):
-    
+print(fnames)
+print(os.getcwd())
+
+def do_compression(fname):
     images_path = "images/"
     
     fpath = images_path + fname
     img = read_image(fpath)
-    
-    
+
     compressed_fpath = images_path + "compressed_" + fname
     compress(img, compressed_fpath)
     compressed_img = read_image(compressed_fpath)
     
-    
     uncompressed_img_size = round((os.stat(fpath).st_size / 1024**2), 4)
     compressed_img_size = round((os.stat(compressed_fpath).st_size / 1024**2), 4)
     compression_ratio = int(100 - (100 * (compressed_img_size / uncompressed_img_size)))
+    
+    #To display compressed size in kb or mb
+    size_unit = "mb"
+    if compressed_img_size < 1:
+        compressed_img_size *= 1024
+        size_unit = "kb"
+        
+    print(f"Original size: {uncompressed_img_size}mb Compressed size: {compressed_img_size}{size_unit}")
     print(f"Compression Ratio: {compression_ratio}% - {fname}: {compressed_img.format}")
     
-for fname in fnames:
-    if "compressed" not in fname:
-        main(fname)
+    merged_img = merge(img, compressed_img)
+    merged_img.show()
+    
+fname = "gear4.jpg"  #assumption is that the picture is in the images folder
+do_compression(fname)
+
