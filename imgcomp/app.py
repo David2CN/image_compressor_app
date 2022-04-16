b72006b5e0043062cfa27f7bfea73cbe8a35849d
David Onyeali <onyealidavidcn@gmail.com>
2022-04-16 14:14:55 +0100

    improved UI with bootstrap and changed project structure

---
+++ imgcomp/app.py
@@ -0,0 +1,75 @@
+import os
+from flask import Flask, flash, request, redirect, render_template, send_from_directory, url_for
+from werkzeug.utils import secure_filename
+from imgcomp.compressor import do_compression, clear_images, get_resolution
+
+abspath = os.getcwd()
+img_path = "static/images/"
+UPLOAD_FOLDER = os.path.join(abspath, img_path)
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+STORAGE_DURATION_LIMIT = 5   # minutes until created files are start to get deleted
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+secret_key = "abd564"
+app.config['SECRET_KEY'] = secret_key
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+
+        # clear old files from storage
+        clear_images(STORAGE_DURATION_LIMIT)
+
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            img_filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(img_filepath)
+
+            compressed_img_filepath = os.path.join(app.config['UPLOAD_FOLDER'], "compressed_"+filename)
+            img_size, compressed_img_size, compressed_rate = do_compression(filename)
+
+            # get filepaths starting from the static directory
+            # for both original and compressed image because
+            # HTML templates seem to require it to get the correct url
+            img_filepath = "static"+img_filepath.split("static")[1]
+            compressed_img_filepath = "static"+compressed_img_filepath.split("static")[1]
+            img_resolution = get_resolution(img_filepath)
+
+            return render_template("compression.html",
+                                   img_filepath=img_filepath,
+                                   compressed_img_filepath=compressed_img_filepath,
+                                   img_size=img_size,
+                                   compressed_img_size=compressed_img_size,
+                                   compressed_rate=compressed_rate,
+                                   compressed_file_name="compressed_"+filename,
+                                   img_resolution=img_resolution)
+
+    return render_template("index.html")
+
+
+@app.route('/static/images/<file_name>', methods=['GET', 'POST'])
+def download_file(file_name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], file_name, as_attachment=True)
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ imgcomp/compressor.py
@@ -0,0 +1,123 @@
+import os
+import time
+from PIL import Image
+
+
+abspath = os.getcwd()
+directory = "static/images/"
+IMG_PATH = os.path.join(abspath, directory)
+
+
+def read_image(img_path):
+    """Read in image and handle error if image is unreadable.
+    """
+    try:
+        img = Image.open(img_path)
+    except OSError:
+        return "Invalid file type!, valid file types include:\
+.jpg, .png, .jpeg, .gif, etc"
+
+    return img
+
+
+def get_paths(filename, images_path=IMG_PATH):
+    filepath = images_path + filename
+    compressed_filepath = images_path + "compressed_" + filename
+
+    return filepath, compressed_filepath
+
+
+def get_size_units(img_size):
+    # To get uncompressed size in kb or mb
+    img_size /= 1024  # first convert to KB
+
+    img_size_unit = "kb"
+    if img_size >= 1024:   # convert the file to mb if the file is greater than 1024kb (1mb)
+        img_size /= 1024
+        img_size_unit = "mb"
+
+    return f"{img_size: .2f}{img_size_unit}"
+
+
+def compress(img, compressed_img_path=directory+"compressed.jpg"):
+    """Compress image.
+    """
+    extension = img.format
+
+    if extension == "PNG":
+        img.save(compressed_img_path, "PNG", quality=10)
+
+    elif extension == "JPEG":
+        img.save(compressed_img_path, "JPEG", quality=50)
+
+    elif extension == "GIF":
+        img.save(compressed_img_path, "GIF", quality=50)
+
+    else:
+        img.save(compressed_img_path, "JPEG", quality=50)
+
+    compressed_img = read_image(compressed_img_path)
+    return compressed_img
+
+
+def do_compression(filename):
+    filepath, compressed_filepath = get_paths(filename)
+
+    # read in image, compress and read in compressed image
+    img = read_image(filepath)
+    compressed_img = compress(img, compressed_filepath)
+
+    # get compression ratio in bytes
+    uncompressed_img_size = os.path.getsize(filepath)
+    compressed_img_size = os.path.getsize(compressed_filepath)
+    compression_ratio = int((1 - (compressed_img_size / uncompressed_img_size)) * 100)
+
+    # get sizes in correct units for display
+    img_size_with_unit = get_size_units(uncompressed_img_size)
+    compressed_img_size_with_unit = get_size_units(compressed_img_size)
+
+    print(f"Original size: {img_size_with_unit} Compressed size: {compressed_img_size_with_unit}")
+    print(f"Compression Ratio: {compression_ratio}% - {filename}: {compressed_img.format}")
+
+    return img_size_with_unit, compressed_img_size_with_unit, compression_ratio
+
+
+def get_resolution(image_filepath):
+    img_res = read_image(image_filepath).size
+    return f"{img_res[0]}x{img_res[1]}"
+
+
+def merge(img1, img2):
+    w = img1.size[0] + img2.size[0]
+    h = max(img1.size[1], img2.size[1])
+
+    merged_image = Image.new("RGBA", (w, h))
+
+    merged_image.paste(img1)
+    merged_image.paste(img2, (img1.size[0], 0))
+
+    return merged_image
+
+
+def clear_images(limit=5):
+    """To delete images that have exceeded storage time
+    """
+    try:
+        files = os.listdir(IMG_PATH)
+        for file in files:
+            if file not in ["logo.ico", "logo.png"]:
+                filepath = os.path.join(IMG_PATH, file)
+
+                time_created = os.path.getctime(filepath)
+                now = time.time()
+
+                duration = now - time_created
+                if duration >= limit*60:
+                    # delete file
+                    os.remove(filepath)
+
+                    print("Storage cleared!")
+    except FileNotFoundError:
+        pass
+    except:
+        pass
